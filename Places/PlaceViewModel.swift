d570d9286f97c35ad9e3382422fe4260093fe4c9
BrunoRiv73 <a01571663@tec.mx>
2025-08-20 18:03:23 -0600

    Now uses loadAPI function to obtain JSON data

--- Places/PlaceViewModel.swift
+++ Places/PlaceViewModel.swift
@@ -18,20 +18,7 @@ class PlaceViewModel {
         Task {
             try await loadAPI()
         }
-        
-        arrPlaces = load("placesData.json")
-        
-        print(arrPlaces)
-        
-       /* var place = Place(name: "ChichenItza", title: "Chichen Itza", description: "Chichen Itza is a famous ancient Mayan city in Mexico, known for its impressive pyramid Kukulkan. It was a major center of politics, religion, and astronomy, and is now one of the New Seven Wonders of the World.", videoURL: "https://www.youtube.com/watch?v=sO7U78pUr34", imageName: ["ChichenItza", "ChichenItza2", "ChichenItza3"])
-        arrPlaces.append(place)
-        
-        place = Place(name: "TajMahal", title: "Taj Mahal", description: "Emperor Shah Jahan in memory of his wife Mumtaz Mahal. It is considered one of the most beautiful architectural masterpieces in the world,", videoURL: "https://www.youtube.com/watch?v=Vu8kO9qxG4o", imageName: ["TajMahal", "TajMahal2", "TajMahal3"])
-        arrPlaces.append(place)
-        
-        place = Place(name: "Colosseum", title: "Roman Colosseum", description: "The Colosseum was built in the 1st century AD in Rome. It hosted gladiator battles, public spectacles, and events, and remains one of the most iconic monuments of the Roman Empire.", videoURL: "https://www.youtube.com/watch?v=pJOwI-74xwY", imageName: ["Colosseum", "Colosseum2", "Colosseum3"])
-        arrPlaces.append(place)*/
-   
+
     }
 
     func load<T: Decodable>(_ filename: String) -> T {
